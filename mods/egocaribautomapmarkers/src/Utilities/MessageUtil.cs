efbe6f0b23a89d7db4a0fdf96bd7b97114f45ee9
egocarib <egocaribx@gmail.com>
2021-07-08 19:44:16 -0500

    New utility for logging/etc

---
+++ mods/egocaribautomapmarkers/src/Utilities/MessageUtil.cs
@@ -0,0 +1,33 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+
+namespace Egocarib.AutoMapMarkers.Utilities
+{
+    public class MessageUtil
+    {
+        /// <summary>
+        /// Send a chat message. If called from the Server thread, serverPlayer must be specified.
+        /// </summary>
+        public static void Chat(string message, IPlayer serverPlayer = null, EnumChatType chatTypeForServer = EnumChatType.Notification)
+        {
+            MapMarkerMod.CoreServerAPI?.SendMessage(serverPlayer, GlobalConstants.GeneralChatGroup, message, chatTypeForServer);
+            MapMarkerMod.CoreClientAPI?.SendChatMessage(message);
+        }
+
+        /// <summary>
+        /// Logs a message to the client or server logs, depending on side of calling thread.
+        /// </summary>
+        public static void Log(string message, EnumLogType type = EnumLogType.Notification)
+        {
+            MapMarkerMod.CoreAPI.Logger.Log(type, $"[Auto Map Markers] {message}");
+        }
+
+        /// <summary>
+        /// Logs an error to the client or server logs.
+        /// </summary>
+        public static void LogError(string message)
+        {
+            Log($"[Auto Map Markers] {message}", EnumLogType.Error);
+        }
+    }
+}
