fcba41044550da1748ee3790cc020550d9c46946
egocarib <egocaribx@gmail.com>
2023-05-27 11:29:46 -0500

    Initial upload: Immersive Corpse Drop v1.0

---
+++ mods/immersivecorpsedrop/src/ImmersiveCorpseDropMod.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace Egocarib.ImmersiveCorpseDrop
+{
+    public class ImmersiveCorpseDropMod : ModSystem
+    {
+        //public override bool ShouldLoad(EnumAppSide forSide) => forSide == EnumAppSide.Client;
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            base.StartClientSide(api);
+
+#if DEBUG
+            Harmony.DEBUG = true;
+#endif
+
+            Harmony harmony = new Harmony("egocarib:ImmersiveCorpseDropMod");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            base.StartServerSide(api);
+
+#if DEBUG
+            Harmony.DEBUG = true;
+#endif
+
+            Harmony harmony = new Harmony("egocarib:ImmersiveCorpseDropMod");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public override void Dispose()
+        {
+            new Harmony("egocarib:ImmersiveCorpseDropMod").UnpatchAll();
+        }
+    }
+}
---
+++ mods/immersivecorpsedrop/src/ImmersiveCorpseDropPatch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+
+namespace Egocarib.ImmersiveCorpseDrop
+{
+    [HarmonyPatch(typeof(EntityBehaviorHarvestable))]
+    public static class ImmersiveCorpseDropPatch
+    {
+        [HarmonyPatch("OnInteract")]
+        [HarmonyPrefix]
+        static bool Prefix(EntityBehaviorHarvestable __instance, EntityAgent byEntity, ref InventoryGeneric ___inv)
+        {
+            bool inRange = (byEntity.World.Side == EnumAppSide.Client && byEntity.Pos.SquareDistanceTo(__instance.entity.Pos) <= 5) || (byEntity.World.Side == EnumAppSide.Server && byEntity.Pos.SquareDistanceTo(__instance.entity.Pos) <= 14);
+            if (!__instance.IsHarvested || !inRange)
+            {
+                return false;
+            }
+            ___inv?.DropAll(__instance.entity.Pos.XYZ);
+            if (__instance.entity.GetBehavior<EntityBehaviorDeadDecay>() != null)
+            {
+                __instance.entity.GetBehavior<EntityBehaviorDeadDecay>().DecayNow();
+            }
+            return false;
+        }
+    }
+}
